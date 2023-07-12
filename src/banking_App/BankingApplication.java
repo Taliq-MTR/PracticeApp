a468ad4b88b647c233125b7637e8d7d158bc132e
Taliq-MTR <mohammadtaliqurrahman82@gmail.com>
2023-07-12 10:58:10 +0530

    made banking package & Classes

---
+++ src/banking_App/BankingApplication.java
@@ -0,0 +1,11 @@
+package banking_App;
+
+public class BankingApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
---
+++ src/banking_App/BankingDatabase.java
@@ -0,0 +1,5 @@
+package banking_App;
+
+public class BankingDatabase {
+
+}
