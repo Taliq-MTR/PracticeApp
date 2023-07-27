651fd1461ec1481908a46f0a54f663dbb3e98ebc
Taliq-MTR <mohammadtaliqurrahman82@gmail.com>
2023-07-27 18:13:02 +0530

    complete menu method.

--- src/banking_App/BankingAccount.java
+++ src/banking_App/BankingAccount.java
@@ -44,6 +44,54 @@ public class BankingAccount {
     }
     
     void menu() {
-    	
+    	char option;
+    	Scanner sc= new Scanner(System.in);
+    	System.out.println("Welcome "+customerName);
+        System.out.println("Your ID:"+customerId);
+        System.out.println("\n");
+        System.out.println("a) Check Balance");
+        System.out.println("b) Deposit Amount");
+        System.out.println("c) Withdraw Amount");
+        System.out.println("d) Previous Transaction");
+        System.out.println("e) Exit");
+        do {
+        	System.out.println("Choose an option");
+        	option=sc.next().charAt(0);
+        	switch(option){
+        		case 'a':
+        			System.out.println("......................");
+                    System.out.println("Balance ="+bal);
+                    System.out.println("......................");
+                    System.out.println("\n");
+                 break;
+        		 case 'b':   
+        			 System.out.println("............");
+        			 System.out.println("Enter the Amount");
+        			 double amt=sc.nextDouble();
+        			 deposit(amt);
+        			 System.out.println("/n");
+        	     break;
+        		 case 'c':
+        			 System.out.println("............");
+        			 System.out.println("Enter the Amount");
+        			 double amtw=sc.nextDouble();
+        			 withdraw(amtw);
+        			 System.out.println("/n");
+
+        		 break;	 
+        		 case 'd':
+        			 
+        		 break;
+        		 case 'e':
+        			 System.out.println("......................");
+        		 break;	 
+        		 default:
+                     System.out.println("Choose a correct option to proceed");	 
+                 break;
+        	}
+        } while (option !='e'); 
+        System.out.println("Thank you for using our banking services");    
     }
+    
+     
 }
