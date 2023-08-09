cf83bbbba53e7cdef97183469e4fbebd7c312a49
Taliq-MTR <mohammadtaliqurrahman82@gmail.com>
2023-08-09 18:37:48 +0530

    Make a logic for user validation

--- src/banking_App/BankingApplication.java
+++ src/banking_App/BankingApplication.java
@@ -8,38 +8,45 @@ public class BankingApplication {
 		Scanner sca = new Scanner(System.in);
 		System.out.println("Please Enter Customer name");
 		String getName = sca.nextLine();
-
+		System.out.println("Enter the Customer Id");
+		int getId = sca.nextInt();
 		BankingDatabase data = new BankingDatabase();
 
 		data.get_customer_data();
+		
+		boolean isUserFound = false;
 		for (int i = 0; i < data.nameList.size(); i++) {
-			if (getName.equals(data.nameList.get(i).getName())) {
-				System.out.println("Enter the Customer Id");
+			if (getName.equals(data.nameList.get(i).getName())  && getId== data.nameList.get(i).getId()) {
+				isUserFound=true;
 				break; // End lopp
 			}
-
-			
+		}
+		
+		if(isUserFound) {
+			System.out.println("Welcome on board...");
+		}else {
+			System.out.println("User not found");
 		}
 		 
 			
 		
 
 
-		int getId = sca.nextInt();
-		data.get_customer_data();
-		for (int i = 0; i < data.nameList.size(); i++) {
-			if (getId == data.nameList.get(i).getId()) {
-//				System.out.println("Enter the Customer Id");
-				break; // End lopp
-			}
+		
+//		data.get_customer_data();
+//		for (int i = 0; i < data.nameList.size(); i++) {
+//			if (getId == data.nameList.get(i).getId()) {
+////				System.out.println("Enter the Customer Id");
+//				break; // End lopp
+//			}
+//
+//			else {
+//				System.out.println("Id not found");
+//			}
+//
+//		}
 
-			else {
-				System.out.println("Id not found");
-			}
-
-		}
-
-		System.out.println("Please Enter Valid Credential name");
+//		System.out.println("Please Enter Valid Credential name");
 
 	}
 
